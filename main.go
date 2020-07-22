39e0a8c774875cc86f7adcd02d46e96e8f2ae6d2
gramilul123 <32932297+gramilul123@users.noreply.github.com>
2020-07-22 17:49:46 +0400

    Google variant

--- main.go
+++ main.go
@@ -1,65 +1,106 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	// MySQL library, comment out to use PostgreSQL.
 	_ "github.com/go-sql-driver/mysql"
+	// PostgreSQL Library, uncomment to use.
+	// _ "github.com/lib/pq"
 )
 
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "ok")
-}
+var db *sql.DB
 
 func main() {
-	http.HandleFunc("/", handle)
-	http.HandleFunc("/_cloudshellProxy/_ah/health", healthCheckHandler)
-	log.Print("Listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	db = DB()
+
+	http.HandleFunc("/", indexHandler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func handle(w http.ResponseWriter, r *http.Request) {
+// DB gets a connection to the database.
+// This can panic for malformed database connection strings, invalid credentials, or non-existance database instance.
+func DB() *sql.DB {
+	var (
+		connectionName = mustGetenv("CLOUDSQL_CONNECTION_NAME")
+		user           = mustGetenv("CLOUDSQL_USER")
+		dbName         = os.Getenv("CLOUDSQL_DATABASE_NAME") // NOTE: dbName may be empty
+		password       = os.Getenv("CLOUDSQL_PASSWORD")      // NOTE: password may be empty
+		socket         = os.Getenv("CLOUDSQL_SOCKET_PREFIX")
+	)
 
-	const dbIP = ""
-	const dbInstanceName = ""
-	const dbName = ""
-	const dbUserName = ""
-	const dbPassword = ""
+	// /cloudsql is used on App Engine.
+	if socket == "" {
+		socket = "/cloudsql"
+	}
 
+	// MySQL Connection, comment out to use PostgreSQL.
+	// connection string format: USER:PASSWORD@unix(/cloudsql/PROJECT_ID:REGION_ID:INSTANCE_ID)/[DB_NAME]
+	dbURI := fmt.Sprintf("%s:%s@unix(%s/%s)/%s", user, password, socket, connectionName, dbName)
+	conn, err := sql.Open("mysql", dbURI)
+
+	// PostgreSQL Connection, uncomment to use.
+	// connection string format: user=USER password=PASSWORD host=/cloudsql/PROJECT_ID:REGION_ID:INSTANCE_ID/[ dbname=DB_NAME]
+	// dbURI := fmt.Sprintf("user=%s password=%s host=/cloudsql/%s dbname=%s", user, password, connectionName, dbName)
+	// conn, err := sql.Open("postgres", dbURI)
+
+	if err != nil {
+		panic(fmt.Sprintf("DB: %v", err))
+	}
+
+	return conn
+}
+
+// indexHandler responds to requests with our list of available databases.
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprint(w, "Hello SQL!  Hello?")
-	fmt.Fprint(w, "\n")
 
-	const dbOpenString = dbUserName + ":" + dbPassword + "@unix(/cloudsql/" + dbInstanceName + ")/" + dbName
+	w.Header().Set("Content-Type", "text/plain")
 
-	db, err := sql.Open("mysql", dbOpenString)
-	defer db.Close()
-
-	err = db.Ping()
+	rows, err := db.Query("SHOW DATABASES")
 	if err != nil {
-		fmt.Fprint(w, "Failed Connection"+"  "+dbOpenString)
-		fmt.Fprint(w, "\n")
-		fmt.Fprint(w, err)
+		log.Printf("Could not query db: %v", err)
+		http.Error(w, "Internal Error", 500)
 		return
-	} else {
-		fmt.Fprint(w, "SUCCESSFUL CONNECTION"+"  "+dbOpenString)
-		fmt.Fprint(w, "\n")
 	}
+	defer rows.Close()
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS exercisecloudsql101 (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(100) NOT NULL, description TEXT, PRIMARY KEY (id))")
-	if err != nil {
-		fmt.Fprint(w, "CREATE TABLE failed:")
-		fmt.Fprint(w, "\n")
-		fmt.Fprint(w, err)
-		fmt.Fprint(w, "\n")
-	} else {
-		fmt.Fprint(w, "SUCCESSFUL CreateTable"+"  "+dbOpenString)
-		fmt.Fprint(w, "\n")
+	buf := bytes.NewBufferString("Databases:\n")
+	for rows.Next() {
+		var dbName string
+		if err := rows.Scan(&dbName); err != nil {
+			log.Printf("Could not scan result: %v", err)
+			http.Error(w, "Internal Error", 500)
+			return
+		}
+		fmt.Fprintf(buf, "- %s\n", dbName)
 	}
-
+	w.Write(buf.Bytes())
 }
+
+func mustGetenv(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		log.Panicf("%s environment variable not set.", k)
+	}
+	return v
+}
+
+// [END gae_cloudsql]
